0556d17c029d6da5a7c592d1a71b3dbc22deaf43
anles <122177638@qq.com>
2020-05-29 10:23:33 +0800

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f  http://zmy122177638.gitee.io/my-blog.git master:gh-pages
+git push -f git@github.com:122177638/my-blog.git master:gh-pages
 
 cd -
